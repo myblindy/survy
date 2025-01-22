3f3317b918bcc47d52d3b5d9ea1a699ddf339243
Marius Mitea <marius.mitea@cae.com>
2025-01-22 14:21:15 -0500

    wave data structure

--- Game/game_state.gd
+++ Game/game_state.gd
@@ -5,11 +5,30 @@ static var player_scene := preload("res://Entities/Actors/Player/player.tscn")
 static var actor_spawner_scene := preload("res://Entities/ActorSpawner/actor_spawner.tscn")
 static var shitty_dude_scene := preload("res://Entities/Actors/ShittyDude/shitty_dude.tscn")
 
-static var spawns := [
-	[0, 4, shitty_dude_scene],
-	[5, 4, shitty_dude_scene],
-	[15, 8, shitty_dude_scene],
-	[25, 8, shitty_dude_scene],
+class WaveData:
+	var wave_length_sec: float
+	var spawns: Array[WaveSpawnData]
+
+	func _init(_wave_length_sec: float, _spawns: Array[WaveSpawnData]) -> void:
+		wave_length_sec = _wave_length_sec
+		spawns = _spawns
+
+class WaveSpawnData:
+	var wave_time_sec: float
+	var actor_count: int
+	var actor_scene: PackedScene
+
+	func _init(_wave_time_sec: float, _actor_count: int, _actor_scene: PackedScene) -> void:
+		wave_time_sec = _wave_time_sec
+		actor_count = _actor_count
+		actor_scene = _actor_scene
+
+static var waves := [
+	WaveData.new(20.0, [
+		WaveSpawnData.new(0, 3, shitty_dude_scene),
+		WaveSpawnData.new(5, 4, shitty_dude_scene),
+		WaveSpawnData.new(10, 6, shitty_dude_scene),
+	]),
 ]
 
 static var width := 2500
--- Scenes/world.gd
+++ Scenes/world.gd
@@ -4,12 +4,15 @@ extends Node2D
 var _time_sec := 0.0
 
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
 	# spawn the player
 	var player: Player = GameState.player_scene.instantiate()
 	player.position = Vector2(GameState.width / 2.0, GameState.height / 2.0)
 	add_child(player)
 	player.add_to_group("player")
 
+var _current_wave_idx := 0
 var _current_spawn_idx := 0
 func _process(delta: float) -> void:
 	_time_sec += delta
@@ -17,17 +20,20 @@ func _process(delta: float) -> void:
 	# time label
 	var time_min := int(_time_sec / 60)
 	var time_sec := fmod(_time_sec, 60)
-	_time_label.text = "Time: %02d:%02d" % [time_min, time_sec]
+	_time_label.text = "Wave %d\n%02d:%02d" % [_current_wave_idx + 1, time_min, time_sec]
 
 	# spawn actors
-	if _current_spawn_idx < len(GameState.spawns):
-		var spawn_data: Array = GameState.spawns[_current_spawn_idx]
-		if _time_sec >= spawn_data[0]:
+	if _current_wave_idx < len(GameState.waves):
+		var wave := GameState.waves[_current_wave_idx]
 
-			for i in range(spawn_data[1]):
-				var spawner: ActorSpawner = GameState.actor_spawner_scene.instantiate()
-				spawner.actor_scene_to_spawn = spawn_data[2]
-				spawner.position = Vector2(randf_range(30, GameState.width - 60), randf_range(30, GameState.height - 60))
-				add_child(spawner)
+		if _current_spawn_idx < len(wave.spawns):
+			var spawn_data := wave.spawns[_current_spawn_idx]
+			if _time_sec >= spawn_data[0]:
 
-			_current_spawn_idx += 1
+				for i in range(spawn_data[1]):
+					var spawner: ActorSpawner = GameState.actor_spawner_scene.instantiate()
+					spawner.actor_scene_to_spawn = spawn_data[2]
+					spawner.position = Vector2(randf_range(30, GameState.width - 60), randf_range(30, GameState.height - 60))
+					add_child(spawner)
+
+				_current_spawn_idx += 1
